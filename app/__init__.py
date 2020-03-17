4aeafd45d1fe3bbe15ff7366e036d47dc97c9650
Christopher Robinson <40131459+MidoriRobin@users.noreply.github.com>
2020-03-17 09:17:43 -0500

    Resolve defeat by a single '

--- app/__init__.py
+++ app/__init__.py
@@ -5,7 +5,7 @@ from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = './app/static/uploads'
 app.config['SECRET_KEY'] = "GazaHaveMoreFans"
-app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://vzmxdkzvlzvqor:345f5c81405d9bccc3c12432947a32ad7ead9dd17f18f256daf751e925029318@ec2-54-152-175-141.compute-1.amazonaws.com:5432/d5qvnef7dkrpv8'"
+app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://vzmxdkzvlzvqor:345f5c81405d9bccc3c12432947a32ad7ead9dd17f18f256daf751e925029318@ec2-54-152-175-141.compute-1.amazonaws.com:5432/d5qvnef7dkrpv8"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
 
 db = SQLAlchemy(app)
