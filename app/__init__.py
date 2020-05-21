3c69f119770bd7d8dfe8f051751f955eb13997dc
Christopher Robinson <40131459+MidoriRobin@users.noreply.github.com>
2020-05-21 00:38:27 -0500

    adujst for heroku push

--- app/__init__.py
+++ app/__init__.py
@@ -6,7 +6,8 @@ import jwt
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = './app/static/uploads'
 app.config['SECRET_KEY'] = "IHaveBeenAtUWIForTooLongINeedToLeave"
-app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://proj2:proj2@localhost/proj2"
+#app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://proj2:proj2@localhost/proj2"
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://yugkiswqtwyxdp:801b2ad8f8ed929dad860261a06ec9de6692b4316dfec738bb0b8d5becbc3226@ec2-3-91-139-25.compute-1.amazonaws.com:5432/dd3482on5hq33d'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
 
 #Flask-Login login LoginManager
